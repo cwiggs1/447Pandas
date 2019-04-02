b92dca9ef17b35843e94e39f05c5487600efc60a
cwiggs1 <cwiggs1@umbc.edu>
2019-04-01 20:51:50 -0400

    added Class structures to match the E-R Diagram submitted with Sprint 1

--- JHU_Scheduler.cpp
+++ JHU_Scheduler.cpp
@@ -1,15 +1,58 @@
 #include <Arduino.h>
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 
-void setup() {
-
+class Employee
+{
+	public:
+	int ID;
+	string Name;
+	bool CanMoonlight;
+	int WeeklyHourReq;
+	int ShiftCount;
 }
 
-void loop() {
-
+Class Schedule
+{
+	public:
+	int ScheduleID;
+	time_t StartDate;
+	time_t EndDate;
 }
+
+Class Event
+{
+	private:
+	int TypeKey;
+	string TypeDescr;
+
+	public:
+	int EventID;
+	time_t StartTime;
+	time_t EndTime
+	string EmployeeName;
+	string Description
+	int Priority
+}
+
+Class Location
+{
+	public:
+	string Name;
+}
+
+class ShiftOpen
+{
+	public:
+	time_t StartTime;
+	time_t EndTime;
+}
+
 
 int main(int argc, const char* argv[])
 {
-	printf("let the games begin\n");
+	printf("Welcome to Scheduler!\n");
+
+	return 0;
 }
