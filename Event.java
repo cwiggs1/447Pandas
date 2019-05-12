cac999bab0644e2266b1efc9750217ea65887540
akashumbc <32750489+akashumbc@users.noreply.github.com>
2019-05-12 17:17:20 -0400

    Add files via upload

---
+++ Event.java
@@ -0,0 +1,14 @@
+import java.sql.Time;
+
+public class Event {
+
+	private int typeKey;
+	private String typeDescr;
+	
+	public int eventID;
+	public Time startTime;
+	public Time endTime;
+	public String employeeName;
+	public String description;
+	public int priority;
+}
