3b94ece56f7f1fad46705895c8aa73ab911d6dd1
Darian Garcia <dgarcia@Darians-MacBook-Pro.local>
2019-05-13 12:39:31 -0400

    This is my work

--- Employee.java
+++ Employee.java
@@ -1,9 +1,77 @@
+//7AM-3:30PM
+//3:30PM-12AM
+
+//SAT AND SUN ARE EVENINGS ONLY
 
 public class Employee {
 
-	public int ID;
-	public String name;
-	public boolean canMoonlight;
-	public int weeklyHoursReq;
-	int shiftCount;
+    private int empl_id;
+    private String name;
+    private boolean moonlighter;
+    private int shift_count;
+    private int hours_count;
+    private int max_hrs;
+    private int[] priorites;
+
+    Employee(int empl_id, String name, boolean moonlighter){
+        this.empl_id = empl_id;
+        this.name = name;
+        this.moonlighter = moonlighter;
+        this.shift_count = 0; //start w/ zero
+        this.hours_count = 0; //start w/ zero
+        this.max_hrs = 40;
+        int NUM_SHIFTS = 19;
+        this.priorites = new int[NUM_SHIFTS];
+    }
+
+    // getters and setters
+    public int getEmpl_id() {
+        return empl_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isMoonlighter() {
+        return moonlighter;
+    }
+
+    public int getShift_count() {
+        return shift_count;
+    }
+
+    public int[] getPriorites() {
+        return priorites;
+    }
+
+    public void setEmpl_id(int empl_id) {
+        this.empl_id = empl_id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMoonlighter(boolean moonlighter) {
+        this.moonlighter = moonlighter;
+    }
+
+    public void setShift_count(int shift_count) {
+        this.shift_count = shift_count;
+    }
+
+    public void setPriority(int index, int value){
+        priorites[index] = value;
+    }
+
+    public int getPriority(int index){
+        return priorites[index]; // value = priorities[index]
+    }
+
+    //question?
+
+
+    // methods
+
 }
--- Shift.java
+++ Shift.java
@@ -1,29 +1,46 @@
-import java.util.PriorityQueue;
-
 public class Shift {
+    private int shift_id; // 0-18 (19 shifts total)
+    private int time_slot; // 1-3 (mo-fri) & 4-5 (sa-sun)
+    private String day; // mo-fri
 
-	private PriorityQueue shiftQueue;
-	private String day;
-	private int shiftNumber;
-	private int id;
-	
-	public Shift(String d, int sn, int ID) {
-		
-		shiftQueue = new PriorityQueue<>();
-		day = d;
-		shiftNumber = sn;
-		id = ID;
-	}
-	
-	public boolean isEmpty() {
-		return shiftQueue.isEmpty();
-	}
-	
-	public void insert() {
-		shiftQueue.add(null);
-	}
-	
-	public void delete() {
-		
-	}
+    Shift(int shift_id, int time_slot, String day){
+        this.shift_id = shift_id;
+        this.time_slot = time_slot;
+        this.day = day;
+    }
+
+    public String toString() {
+        return "\n \nshift_id: " + this.shift_id + " \ntime_slot: " + this.time_slot + " | day: " + this.day ;
+    }
+
+    //  getters & setters
+    public int getShift_id() {
+        return shift_id;
+    }
+
+    public int getTime_slot() {
+        return time_slot;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setShift_id(int shift_id) {
+        this.shift_id = shift_id;
+    }
+
+    public void setTime_slot(int time_slot) {
+        this.time_slot = time_slot;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    // methods
+
 }
+
+
+
