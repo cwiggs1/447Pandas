43a69b9da9f3395238e4f5aad80ed4dc1bfa1313
dsteward2 <43303142+dsteward2@users.noreply.github.com>
2019-05-15 14:30:04 -0400

    Added employ_id and getter and setter

--- Shift.java
+++ Shift.java
@@ -1,46 +1,53 @@
 public class Shift {
-    private int shift_id; // 0-18 (19 shifts total)
-    private int time_slot; // 1-3 (mo-fri) & 4-5 (sa-sun)
-    private String day; // mo-fri
+  private int shift_id; // 0-18 (19 shifts total)
+  private int time_slot; // 1-3 (mo-fri) & 4-5 (sa-sun)
+  private String day; // mo-fri
+  private int employ_id; // the id of the employee that this belongs to
 
-    Shift(int shift_id, int time_slot, String day){
-        this.shift_id = shift_id;
-        this.time_slot = time_slot;
-        this.day = day;
-    }
+  Shift(int shift_id, int time_slot, String day){
+    this.shift_id = shift_id;
+    this.time_slot = time_slot;
+    this.day = day;
+    this.employ_id = -1
+  }
 
-    public String toString() {
-        return "\n \nshift_id: " + this.shift_id + " \ntime_slot: " + this.time_slot + " | day: " + this.day ;
-    }
+  public String toString() {
+    return "\n \nshift_id: " + this.shift_id + " \ntime_slot: " + this.time_slot + " | day: " + this.day ;
+  }
 
-    //  getters & setters
-    public int getShift_id() {
-        return shift_id;
-    }
+  //  getters & setters
+  public int getEmploy_id() {
+    return employ_id;
+  }
 
-    public int getTime_slot() {
-        return time_slot;
-    }
+  public int getShift_id() {
+    return shift_id;
+  }
 
-    public String getDay() {
-        return day;
-    }
+  public int getTime_slot() {
+    return time_slot;
+  }
 
-    public void setShift_id(int shift_id) {
-        this.shift_id = shift_id;
-    }
+  public String getDay() {
+    return day;
+  }
 
-    public void setTime_slot(int time_slot) {
-        this.time_slot = time_slot;
-    }
+  public void setEmploy_id(int employ_id) {
+    this.employ_id = employ_id;
+  }
 
-    public void setDay(String day) {
-        this.day = day;
-    }
+  public void setShift_id(int shift_id) {
+    this.shift_id = shift_id;
+  }
+
+  public void setTime_slot(int time_slot) {
+    this.time_slot = time_slot;
+  }
+
+  public void setDay(String day) {
+    this.day = day;
+  }
 
     // methods
 
 }
-
-
-
