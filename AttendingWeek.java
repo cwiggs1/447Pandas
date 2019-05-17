6ef52daf3452e093079e6cb983ec312c2d67f5d3
cwiggs1 <cwiggs1@umbc.edu>
2019-05-17 16:01:16 -0400

    Added AWconstraints to the Employee class and a helper function to set it up correctly for the AttendingWeek class
    AttendingWeek.java currently contains the algorithm that will put employees into a schedule of 13 weeks based first on their average availability for each week. If that is equal (everyone is available) the algorithm chooses the employee with the least num_attending_weeks (most likely least available). Currently does not account for unlikely edge cases

---
+++ AttendingWeek.java
@@ -0,0 +1,88 @@
+import java.lang.reflect.Array;
+import java.util.Objects;
+import java.util.ArrayList;
+
+
+
+public Class AttendingWeek {
+
+    public static int daysPerPeriod = 65;
+    public static int weeksPerPeriod = 13;
+
+
+    private int[13] schedule;
+
+    //get an array of approx 65 ints from each employee
+    //those ints are constraints 0 being can't work, 3 being available
+    //employee has num_attending_weeks that is an int (0-13) of weeks where they can work all shifts
+    public int[] ScheduleAttendingWeeks(int[] employees) {  //inside the employee array should be a variable with a 65 int array
+
+        int id = employees[i].getEmpl_id();
+        int AWnum = employees[i].getNumAttendingWeeks();  //the lower this value the higher priority
+
+
+        //check which employee has the lowest num_attending_weeks
+        int lowestAW = employees[0].getNumAttendingWeeks();
+        Employee priorityEmpl;
+
+         for (int i = 1; i < len(employees); i++)  //finds the lowest num attending weeks to know which employee to schedule
+        {
+            if(employees[i].getNumAttendingWeeks() < lowestAW && employees[i].getNumAttendingWeeks() > 0)  
+            {
+                lowestAW = employees[i].getNumAttendingWeeks();  
+                priorityEmpl = employees[i];
+                setNumAttendingWeek(14);  //resets the number this calculation is based on so the same employee won't be picked every time
+            }
+        }
+
+        //assign them the earliest shift they can work
+        int emplConstraints[];
+        List<Employee> free[];
+        Employee leastConstrained = employees[0];
+        int lowestConstraint = 0;  //initialize to highest value
+        boolean noFree = false;
+
+        for (int i = 0; i < weeksPerPeriod; i++)  //for loop going through each week of a period
+        {
+            for (int j = 0; j < len(employees); j++)  //for loop going through each employee
+            {
+                emplConstraints[j] = employees[j].getAWconstraints();
+                if (emplConstraints[j] == 3)  //adds any employee that is fully available for an AW to the 'free' list
+                {
+                    free.add(employees[j]);
+                    noFree = false;
+                }
+                else if (emplConstraints[j] < 3 && emplConstraints[j] > lowestConstraint)  //if no one is fully available it will assign 'least constrained' empl to work
+                {
+                    leastConstrained = employees[j];
+                    lowestConstraint = emplConstraints[j];
+                    schedule[i] = leastConstrained.getEmpl_id();
+                    noFree = true;
+                }
+            }
+
+            if (!noFree)  //if no Free is true the schedule was correctly updated already and the next for loop isn't necessary
+            { 
+                priorityEmpl = free[0];
+
+                for (int k = 1; k < free.size(); k++)  //finds the free employee with the least availability
+                {
+                    if (employees[k].getNumAttendingWeeks() < priorityEmpl.getNumAttendingWeeks())
+                    {
+                        priorityEmpl = employees[k];
+                    }
+                }
+
+                schedule[i] = priorityEmpl.getEmpl_id();  //assigns employee to work 
+            }
+
+            //must reset some values for next iteration of loop
+            lowestConstraint = 0;
+            free.clear();  
+            noFree = false;
+        }
+
+        return schedule;
+    }
+
+}
--- Employee.java
+++ Employee.java
@@ -1,9 +1,188 @@
+//7AM-3:30PM
+//3PM-11:30PM
+//11PM-7:30AM
+
+//SAT AND SUN ARE EVENINGS ONLY
 
 public class Employee {
 
-	public int ID;
-	public String name;
-	public boolean canMoonlight;
-	public int weeklyHoursReq;
-	int shiftCount;
+  private int empl_id;
+  private String name;
+  private boolean moonlighter;
+  private int shift_count;
+  private int hours_count;
+  private int max_hrs;
+  private int[] priorites;
+  private double[] morning_priorites;
+  private double avg_priorites;
+  private int num_attending_weeks;
+  private double avg_morning_priority;
+  private double[] AWconstraints;  //should be 13 doubles with the average priority availability for each week in the period in order
+
+  public static int weeksPerPeriod = 13;
+
+  Employee(int empl_id, String name, boolean moonlighter){
+    this.empl_id = empl_id;
+    this.name = name;
+    this.moonlighter = moonlighter;
+    this.shift_count = 0; //start w/ zero
+    this.hours_count = 0; //start w/ zero
+    this.max_hrs = 40;
+    int NUM_SHIFTS = 19;
+    this.priorites = new int[NUM_SHIFTS];
+    this.avg_priorites = 0;
+    this.num_attending_weeks = 0;
+    this.avg_morning_priority = 0;
+    int NUM_ATTENDING_WEEKS = 13;
+    this.morning_priorites = new double[NUM_ATTENDING_WEEKS];
+    this.AWconstraints = new double[weeksPerPeriod] = avgConstraintPerWeek(priorites);  //should set AW constraints to the avg constraint value per week
+  }
+
+  // getters and setters
+  public void setAWconstraints(int index, int value)
+  {
+    AWconstraints[index] = value;
+  }
+  public void setAvgMorningPriority(double number) {
+    this.avg_morning_priority = number;
+  }
+
+  public void setMorningPriority(int index, double number) {
+    this.morning_priorites[index] = number;
+  }
+
+  public double getMorningPriority(int index) {
+    return morning_priorites[index];
+  }
+
+  public int getAvgMorningPriority() {
+    return avg_morning_priority;
+  }
+
+  public int getHours_count() {
+    return hours_count;
+  }
+
+  public double getAvgPriority() {
+    return avg_priorites;
+  }
+
+  public int getNumAttendingWeeks(){
+    return num_attending_weeks;
+  }
+
+  public int getEmpl_id() {
+    return empl_id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public boolean isMoonlighter() {
+    return moonlighter;
+  }
+
+  public int getShift_count() {
+    return shift_count;
+  }
+
+  public int[] getPriorites() {
+    return priorites;
+  }
+
+  public int[] getAWconstraints() { 
+    return AWconstraints;
+  }
+
+  public void setHours_count(int hours_count){
+    this.hours_count = hours_count;
+  }
+
+  public void setAvgPriority(double avg_priorites) {
+    this.avg_priorites = avg_priorites;
+  }
+
+  public void setNumAttendingWeek(int num_attending_weeks) {
+    this.num_attending_week = num_attending_weeks;
+  }
+
+  public void setEmpl_id(int empl_id) {
+    this.empl_id = empl_id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setMoonlighter(boolean moonlighter) {
+    this.moonlighter = moonlighter;
+  }
+
+  public void setShift_count(int shift_count) {
+    this.shift_count = shift_count;
+  }
+
+  public void setPriority(int index, int value){
+    priorites[index] = value;
+  }
+
+  public int getPriority(int index){
+    return priorites[index]; // value = priorities[index]
+  }
+
+  //question?
+
+
+  // methods
+
+  public double findAvgPriority() {
+    for (int i = 0; i < NUM_SHIFTS; i++) {
+      this.avg_priorites += priorites[i];
+    }
+    this.avg_priorites = this.avg_priorites / NUM_SHIFTS;
+    return avg_priorites;
+  }
+
+  public double loopAttendWeekPriority(int numWeeks) {
+    double tempAvg = 0;
+    for (int i = (numWeeks * 19); i <= ((numWeeks * 19) + 15); i += 3) {
+      tempAvg += getPriority(i);
+    }
+    return tempAvg;
+  }
+
+  public double setupAttendingAvgPriority() {
+    double tempNumber = 0;
+    //for the number of weeks during a scheduling session (13 weeks)
+    for (int i = 0; i < 13; i++) {
+      //call attending week function to calculate the average priority over that week
+      tempNumber += loopAttendWeekPriority(i);
+      setMorningPriority(i, tempNumber);
+    }
+    setAvgMorningPriority((tempNumber / 65));
+  }
+
+  public double[] avgContraintPerWeek(double[] constraints) //should set AWconstraints variable in Employee
+  { 
+
+    int weekCount = 0;
+    double weekTotal = 0;
+    double[] constraintPerWeek;  //len should be 13
+
+    for (int i = 0; i < daysPerPeriod; i++) {
+
+      weekTotal += constraints[i];  
+
+        if (i % 5 == 4)  //if its a friday constraint, take the average and store it in the output array
+        {
+            constraintPerWeek[weekCount] = weekTotal / 5;
+            weekTotal = 0;
+            weekCount += 1;
+        }
+    }
+
+    return constraintPerWeek;
+  }
+
 }
