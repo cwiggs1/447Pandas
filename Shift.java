daf14dd392901549e95b73c8d7ea5858a3a6d8aa
dsteward2 <43303142+dsteward2@users.noreply.github.com>
2019-05-20 12:23:20 -0400

    Moved from 'priority_algo'

--- Shift.java
+++ Shift.java
@@ -1,29 +1,147 @@
-import java.util.PriorityQueue;
-
 public class Shift {
+  private int shift_id; // 0-18 (19 shifts total)
+  private int time_slot; // 1-3 (mo-fri) & 4-5 (sa-sun)
+  private String day; // mo-fri
+  private int employ_id; // the id of the employee that this belongs to
+  private LocalDateTime StartDateTime;
+  private LocalDateTime EndDateTime;
 
-	private PriorityQueue shiftQueue;
-	private String day;
-	private int shiftNumber;
-	private int id;
-	
-	public Shift(String d, int sn, int ID) {
-		
-		shiftQueue = new PriorityQueue<>();
-		day = d;
-		shiftNumber = sn;
-		id = ID;
-	}
-	
-	public boolean isEmpty() {
-		return shiftQueue.isEmpty();
-	}
-	
-	public void insert() {
-		shiftQueue.add(null);
-	}
-	
-	public void delete() {
-		
-	}
+  Shift(int shift_id, int time_slot, String day){
+    this.shift_id = shift_id;
+    this.time_slot = time_slot;
+    this.day = day;
+    this.employ_id = -1;
+    this.StartDateTime = null;
+    this.EndDateTime = null;
+  }
+
+  public String toString() {
+    return "\n \nshift_id: " + this.shift_id + " \ntime_slot: " + this.time_slot + " | day: " + this.day ;
+  }
+
+  //  getters & setters
+  public int getEmploy_id() {
+    return employ_id;
+  }
+
+  public int getShift_id() {
+    return shift_id;
+  }
+
+  public int getTime_slot() {
+    return time_slot;
+  }
+
+  public String getDay() {
+    return day;
+  }
+
+  public void setEmploy_id(int employ_id) {
+    this.employ_id = employ_id;
+  }
+
+  public void setShift_id(int shift_id) {
+    this.shift_id = shift_id;
+  }
+
+  public void setTime_slot(int time_slot) {
+    this.time_slot = time_slot;
+  }
+
+  public void setDay(String day) {
+    this.day = day;
+  }
+
+  // methods
+
+  public void assignTime(LocalDateTime startDate, int numWeeks) {
+    //will bring in a starting date and time for the period - will be
+    // the 0 index
+
+    //idea: 1) calculate the days using the 'numWeeks' and the type of
+    // shift with a switch statement and within that one switch statement
+    // 2) have it calculate the time based on the type of shift given
+
+    //1) convert weeks to days
+    long newDays = numWeeks * 7;
+
+    //2) figure out the day of the week that this Shift class falls on
+    int dayOfWeek = seekDOW();
+
+    //3) change the date with add 'newDays' and 'dayOfWeek'
+    newDays += dayOfWeek;
+
+    //4) use 'plusDays(newDays)' to make a new LocalDateTime for the Start and End
+    LocalDateTime newDateTime = startDate.plusDays(newDays);
+
+    //5) now need to find out the timeslot
+    // findTimeSlot();
+
+    //6) convert the time_slot to actual times
+    convertTime(newDateTime.toLocalDate());
+  }
+
+  //this will load up the start and end times for those days
+  public LocalTime convertTime(LocalDate date) {
+    LocalTime time;
+    String timeString_1;
+    String timeString_2;
+
+    if (this.time_slot == 1) {
+      timeString_1 = "07:00";
+      timeString_2 = "15:30";
+    }
+    else if (this.time_slot == 2) {
+      timeString_1 = "15:00";
+      timeString_2 = "23:30";
+    }
+    else if (this.time_slot == 3) {
+      timeString_1 = "23:00";
+      timeString_2 = "07:30";
+      date = LocalDate.plusDays(1);
+    }
+    else if (this.time_slot == 4) {
+      timeString_1 = "07:00";
+      timeString_2 = "19:00";
+    }
+    else if (this.time_slot == 5) {
+      timeString_1 = "19:00";
+      timeString_2 = "07:00";
+      date = LocalDate.plusDays(1);
+    }
+
+    time = LocalTime.parse(timeString_1);
+
+    this.StartDateTime = LocalDateTime.of(time, date);
+
+    time = LocalTime.parse(timeString_2);
+
+    this.EndDateTime = LocalDateTime.of(time, date)
+  }
+
+  public void findTimeSlot() {
+    if (this.shift_id < 15) {
+      this.time_slot = (this.shift_id % 3) + 1;
+    }
+    else {
+      if (this.shift_id % 2 == 1) {
+        this.time_slot = 4;
+      }
+      else {
+        this.time_slot = 5;
+      }
+    }
+  }
+
+  public int seekDOW(){
+
+    int day = 0;
+
+    for (int i = 3; i < 21; i += 3) {
+      if (this.shift_id < i) {
+        return day;
+      }
+      day += 1;
+    }
+  }
 }
--- csv_exporter.java
+++ csv_exporter.java
@@ -10,18 +10,18 @@ import java.text.*;
 import java.time.*;
 
 /*
-  What needs to be included for CSV:
-    - Employee.Name
+What needs to be included for CSV:
+- Employee.Name
 
-    - Event.Description
-    - Event.StartDate
-    - Event.EndDate
+- Event.Description
+- Event.StartDate
+- Event.EndDate
 
-    - Location.Name (Remember to include quotes around the Location should there be a comma in between)
+- Location.Name (Remember to include quotes around the Location should there be a comma in between)
 */
 
 
-//This Class will require Lists of the employees (or just one for the single file for the regular employee), events, and the Location.Name
+//This Class will require Lists of the employees (or just one for the single file for the regular employee), shifts, and the Location.Name
 // It will also require the change of the Event class to be within the Employee class in an array and each one should include a "LocalDateTime" variable that can be parsed for date and time of the shift the employee has
 public class OpenCSWriter{
 
@@ -30,15 +30,15 @@ public class OpenCSWriter{
 
   NEEDS:
   - Array of Employee objects
-  - Array of events
+  - Array of shifts
   - Array of Location objects
   - How many iterations of Employees it muct do (just do employeeArray.length)
   */
-  public OpenCSWriter_Master(Employee[] currEmploys, Event[] events, Location[] locations, int iterations) {
+  public OpenCSWriter_Master(Employee[] currEmploys, Shift[] shifts, int iterations) {
 
     for (int e = iterations; e > 0; e--) {
 
-      String filePath = "c://documents//" + Integer.toString(currEmploys[e].ID) + "_schedule.csv";
+      String filePath = "c://documents//" + Integer.toString(currEmploys[e].empl_id) + "_schedule.csv";
       File file = new File(filePath);
       try (
       FileWriter outputfile = new FileWriter(file);
@@ -52,11 +52,11 @@ public class OpenCSWriter{
 
       //This for loop assumes that the event times match the schedule dates...
       String start_time, end_time, start_date, end_date;
-      for (int i = events.length - 1; i >= 0; i--) {
+      for (int i = shifts.length - 1; i >= 0; i--) {
 
         //check to see if the current event belongs to this employee
-        if (event[i].EmployeeName != currEmploys[e].Name) {
-          System.out.println("This event - "+ event[i].EventID+" - does not match the Employee, " +currEmploys[e].Name);
+        if (shifts[i].employ_id != currEmploys[e].empl_id) {
+          System.out.println("This event - "+ shifts[i].shift_id+" - does not match the Employee, " +currEmploys[e].Name);
           continue;
         }
 
@@ -65,36 +65,36 @@ public class OpenCSWriter{
         */
 
         //'*1000' converts it to miliseconds from seconds
-        start_time = (event[i].StartTime).format(timeFormat);
-        start_date = (event[i].StartTime).format(dateFormat);
+        start_time = (shifts[i].StartTime).format(timeFormat);
+        start_date = (shifts[i].StartTime).format(dateFormat);
 
-        end_time = (event[i].EndTime).format(timeFormat);
-        end_date = (event[i].EndTime).format(dateFormat);
+        end_time = (shifts[i].EndTime).format(timeFormat);
+        end_date = (shifts[i].EndTime).format(dateFormat);
 
         /*
         Section starts putting stuff into the array and then it is written in
         */
 
         //assumes that the location array is the same length as the event array
-        String[] line {events[i].Subject, start_date, start_time, end_date, end_time, events[i].Description,
-          locations[i].Name};
-          writer.writeNext(line);
+        String[] line {"Shift #" + (Integer.toString(shifts[i].getShift_id())),
+        start_date, start_time, end_date, end_time};
+        writer.writeNext(line);
 
-        }
+      }
 
-        /*
-        Done writing to file, so end the connection to file
-        */
-        writer.close();
+      /*
+      Done writing to file, so end the connection to file
+      */
+      writer.close();
 
-        )
+      )
 
-        catch (IOException | ArrayIndexOutOfBoundsException e) {
-          e.printStackTrace();
-        }
-
+      catch (IOException | ArrayIndexOutOfBoundsException e) {
+        e.printStackTrace();
       }
 
     }
 
   }
+
+}
